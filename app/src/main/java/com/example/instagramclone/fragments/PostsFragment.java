299ec65ad3de0f9279cff813038f7a136a34dd51
fzaman2258 <fzaman2258@bths.edu>
2021-03-17 00:52:44 -0400

    Home Fragment fetches posts data

--- app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/PostsFragment.java
@@ -2,22 +2,34 @@ package com.example.instagramclone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.instagramclone.Post;
+import com.example.instagramclone.PostsAdapter;
 import com.example.instagramclone.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PostsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class PostsFragment extends Fragment {
 
-
+    private RecyclerView rvPosts;
+    public static final String TAG = "PostsFragment";
+    private PostsAdapter adapter;
+    private List<Post> allPosts;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -29,5 +41,40 @@ public class PostsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
+
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        queryPosts();
+
+    }
+
+    private void queryPosts() {
+
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG,"Issues with getting posts", e);
+                    return;
+                }
+                for(Post post:posts){
+                    Log.i(TAG,"Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                    allPosts.addAll(posts);
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+        });
     }
 }
